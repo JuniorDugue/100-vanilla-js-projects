d856998cc9ff8320beaf63d82fe23624080071b2
juniordugue <jrdugue@gmail.com>
2020-09-20 03:22:23 -0400

    Added conditionals via js to all of the remaining form fields, and then added js email regex for email validation

--- form-validator/script.js
+++ form-validator/script.js
@@ -14,9 +14,15 @@ function showError(input, message) {
 }
 
 // Show success outline
-function showSuccess(input){
+function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 // Event Listeners
@@ -29,5 +35,25 @@ form.addEventListener("submit", function (e) {
     showError(username, "Username is required");
   } else {
     showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, "Password2 is required");
+  } else {
+    showSuccess(password2);
   }
 });
