d30a7032cb96345d6b537b2a1c96000a53bc11f4
juniordugue <jrdugue@gmail.com>
2020-09-20 18:17:06 -0400

    Added url for the memory game, Created game board and used for loop to loop thru my cards,  added more assets (blank and white  pngs), event listener for when a card is flipped, wrote another if to check if the 2 cards chosen matches

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ small applications, side projects with vanilla js no frameworks
 deployed site(s):
 
 form validator https://form-validator-ten.vercel.app/
+memory game(lambda units edition) https://lambda-units-memory-game.vercel.app/
--- memory-game/index.js
+++ memory-game/index.js
@@ -1,29 +1,54 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // card options
   const cardArray = [
     {
-      name: 'unit1', 
-      img: './assets/lambdaunits-assets/unit1.png'
+      name: "unit1",
+      img: "assets/lambdaunits-assets/unit1.png",
     },
     {
-      name: 'unit2', 
-      img: './assets/lambdaunits-assets/unit2.png'
+      name: "unit2",
+      img: "assets/lambdaunits-assets/unit2.png",
     },
     {
-      name: 'unit3', 
-      img: './assets/lambdaunits-assets/unit3.png'
+      name: "unit3",
+      img: "assets/lambdaunits-assets/unit3.png",
     },
     {
-      name: 'unit4', 
-      img: './assets/lambdaunits-assets/unit4.png'
+      name: "unit4",
+      img: "assets/lambdaunits-assets/unit4.png",
     },
     {
-      name: 'cs', 
-      img: './assets/lambdaunits-assets/cs.png'
+      name: "cs",
+      img: "assets/lambdaunits-assets/cs.png",
     },
     {
-      name: 'labs', 
-      img: './assets/lambdaunits-assets/labs.png'
+      name: "labs",
+      img: "assets/lambdaunits-assets/labs.png",
     },
-  ]
-})
+  ];
+});
+
+const grid = document.querySelector(".grid");
+let cardsChosen = [];
+let cardsChosenId = [];
+
+// gameboard
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    let card = document.createElement("img");
+    card.setAttribute("src", "assets/lambdaunits-assets/blank.png");
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipcard);
+    grid.appendChild(card);
+  }
+}
+
+// card flip
+function flipCard() {
+  let cardId = this.getAttribute("data-id");
+  cardsChosen.push(cardArray[cardId].name);
+  cardsChosenId.push(cardId);
+  this.setAttribute("src", cardArray[cardId].img);
+}
+
+createBoard();
