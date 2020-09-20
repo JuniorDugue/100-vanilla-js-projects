12d5d07bb9081b3bbc60192dee7e532185605073
juniordugue <jrdugue@gmail.com>
2020-09-20 17:40:11 -0400

    Styled basic flex box and added card array with the name and images within objects inside of my event listener

---
+++ memory-game/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // card options
+  const cardArray = [
+    {
+      name: 'unit1', 
+      img: './assets/lambdaunits-assets/unit1.png'
+    },
+    {
+      name: 'unit2', 
+      img: './assets/lambdaunits-assets/unit2.png'
+    },
+    {
+      name: 'unit3', 
+      img: './assets/lambdaunits-assets/unit3.png'
+    },
+    {
+      name: 'unit4', 
+      img: './assets/lambdaunits-assets/unit4.png'
+    },
+    {
+      name: 'cs', 
+      img: './assets/lambdaunits-assets/cs.png'
+    },
+    {
+      name: 'labs', 
+      img: './assets/lambdaunits-assets/labs.png'
+    },
+  ]
+})
