afb52830618cff784e015f9c5a37505f5553b565
juniordugue <jrdugue@gmail.com>
2020-10-15 21:01:09 -0700

    pushed latest changes for the movie seat booking app, still have a few bugs to fix

--- movie-seat-booking/index.js
+++ movie-seat-booking/index.js
@@ -1,2 +1,14 @@
-const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".seat");
+const seatPickers = document.querySelector(".seat-pickers");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+const ticketPrice = +movieSelect.value;
+
+console.log(ticketPrice);
+
+seatPickers.addEventListener("click", (e) => {
+  if (e.target.classList.contains("seat") && !e.target.classList.contains("occupied")) {
+    e.target.classList.toggle("selected");
+  }
+});
---
+++ movie-seat-booking/notes.md
@@ -0,0 +1,6 @@
+# notes
+
+- adding a + or - in front of movieSelect on line 6 changed the value to a number from a string
+
+- 
+
