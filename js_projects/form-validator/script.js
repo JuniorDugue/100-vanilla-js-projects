da8536819b648e290cc9f75b2874b8e0de1079f8
juniordugue <jrdugue@gmail.com>
2020-12-11 21:52:55 -0500

    decided to create a blog for these projects, 100 javascript projects blog using gatsby js

--- js_projects/form-validator/script.js
+++ js_projects/form-validator/script.js
@@ -25,35 +25,16 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach*(function(input){
+    console.log(input.value);
+  })
+}
+
 // Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("submit");
-  console.log(username.value);
 
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
-
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(password);
-  }
-
-  if (password2.value === "") {
-    showError(password2, "Password2 is required");
-  } else {
-    showSuccess(password2);
-  }
+  checkRequired([username, email, password, password2]);
 });
---
+++ notes.md
@@ -0,0 +1,6 @@
+# [notes](https://www.gatsbyjs.com/tutorial/), [tips](https://www.gatsbyjs.com/docs/gatsby-cli/), & [tricks](https://www.gatsbyjs.com/docs/cheat-sheet/) while using [gatsby](https://www.gatsbyjs.com/) for the first time
+
+- [`npm i -g gatsby-cli](https://www.gatsbyjs.com/get-started/)
+- typing `gatsby help` in the terminal will share all of the available commands
+
+- `gatsby new 'project name' ` to create new project, `cd` into the 'project name', then `gatsby develop` to start up the server
